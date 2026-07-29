ae94f8a4e5d556815b4c09038d71b4e4641ae2df
momo <ptjkjm1@gmail.com>
2026-07-29 16:52:47 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Cloud Architecture Deploy Demo
+
+AWS 상에서 안전하게 중단 없이 운영하기 위한 데모 목적의 프로젝트
---
+++ src/main/java/io/github/prjkmo112/cloudarchitecturedeploydemo/CloudArchitectureDeployDemoApplication.java
@@ -0,0 +1,13 @@
+package io.github.prjkmo112.cloudarchitecturedeploydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudArchitectureDeployDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudArchitectureDeployDemoApplication.class, args);
+    }
+
+}
